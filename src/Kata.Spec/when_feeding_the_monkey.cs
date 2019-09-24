01d4cd88bb4184aaa374877b81ccafc202e4d22a
nescoto <nestor@nescoto.me>
2019-09-24 15:46:42 -0600

    Adding test for single input value

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -32,11 +32,26 @@ namespace Kata.Spec
         private static int _result;
     }
 
+    public class when_user_input_is_one_number
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("3"); };
+
+        It should_return_the_number = () => { _result.Should().Be(3); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
+
+
     internal class Calculator
     {
-        public int Add()
+        public int Add(string input="")
         {
-            return - 1;
+            return 0;
         }
     }
 }
