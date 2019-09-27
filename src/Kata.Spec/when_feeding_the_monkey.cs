b689b9a0f6340eef5b0468da2fc73f63dc2e27b8
Isaac Martinez <imtz90b@gmail.com>
2019-09-27 14:18:41 -0600

    one number input passing, two numbers failing

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -42,19 +42,22 @@ namespace Kata.Spec
         private static int _result;
     }
 
-
-    internal class Calculator
+    public class when_input_is_two_numbers
     {
-        public int Add(string input="")
+        Establish _context = () =>
         {
-            return 0;
-        }
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("3,4"); };
+
+        It should_return_the_sum_of_those_numbers = () => { _result.Should().Be(7); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
     }
 }
 
 /*
-1. Given the user input is empty when calculating the sum then it should return zero.
-2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
